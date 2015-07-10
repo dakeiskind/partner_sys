b89b64ecb509796b0af9e960fe498c76ea1ec752
dakeiskind <dakeiskind@sina.com>
2015-07-10 17:11:55 +0800

    common announcement list

---
+++ common/views.py
@@ -0,0 +1,32 @@
+__author__ = 'chenqi'
+
+from django.shortcuts import render_to_response
+from django.http import HttpRequest, HttpResponse, Http404, JsonResponse
+from django.template.loader import get_template
+from django.template import Context
+from common.models import Announcement
+import json
+
+
+#common announcements list
+def anno_list(request, page, size):
+    try:
+        page = int(page)
+        size = int(size)
+    except ValueError:
+        raise Http404
+
+    start = (page - 1) * size
+    offset = start + size
+    common_annos = Announcement.formals.all()[start:offset]
+
+    response_data = {}
+    response_data['title'] = ""
+    response_data['link'] = ""
+    response_data['href'] = ""
+    response_data['datas'] = datas = []
+    for idx,item in enumerate(common_annos):
+        datas.append({'title':item.title, 'href':item.paper_path})
+
+    # return HttpResponse(json.dumps(response_data), content_type="application/json")
+    return JsonResponse(response_data)
--- partner_sys/urls.py
+++ partner_sys/urls.py
@@ -5,10 +5,12 @@ from hello_to_viewflow.views import *
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+import common.views as common_views
 
 urlpatterns = patterns('',
     url(r'^$', index, {'home':'home.html'}),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^common/anno/list/(?P<page>\d+)/(?P<size>\d+)/$', common_views.anno_list),
     url(r'^helloworld/', include([
         HelloWorldFlow.instance.urls,
         url('^$', viewflow.ProcessListView.as_view(), name='index'),
@@ -16,7 +18,9 @@ urlpatterns = patterns('',
         url('^queue/$', viewflow.QueueListView.as_view(), name='queue'),
         url('^details/(?P<process_pk>\d+)/$', viewflow.ProcessDetailView.as_view(), name='details'),
     ], namespace=HelloWorldFlow.instance.namespace), {'flow_cls': HelloWorldFlow}),
-    url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
+    url(r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^imgs/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
 
 
 # urlpatterns += patterns(url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
