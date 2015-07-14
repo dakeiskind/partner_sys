267db45422128442a57d4eab89343a1af02955de
dakeiskind <dakeiskind@sina.com>
2015-07-14 16:04:19 +0800

    user login

--- secur_auth/views.py
+++ secur_auth/views.py
@@ -2,6 +2,7 @@ __author__ = 'chenqi'
 
 from django.http import Http404, HttpRequest, HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.http import *
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 import re
@@ -24,6 +25,8 @@ def sign_up(request):
     user_reg_m = username_regex.match(username)
     if user_reg_m is None or user_reg_m.group() != username:
         return JsonResponse({'error':u'用户名只能包含字母、数字及下划线，且必须字母或数字开头'})
+    if User.objects.get(username=username) is not None:
+        return JsonResponse({'error':u'此用户名已被注册'})
     if password is None or password_confirmed is None:
         return JsonResponse({'error':u'密码不可为空'})
     if password_confirmed != password:
@@ -35,6 +38,8 @@ def sign_up(request):
     email_reg_m = email_regex.match(email)
     if email_reg_m is None or email_reg_m.group() != email:
         return JsonResponse({'error':u'邮箱地址格式不正确'})
+    if User.objects.get(email=email) is not None:
+        return JsonResponse({'error':u'此邮箱已被注册'})
 
     new_user = User.objects.create_user(username, email, password)
     new_user.save()
@@ -49,14 +54,15 @@ def sign_in(request):
 
     user = authenticate(username=username, password=password)
     if user is None:
-        return JsonResponse({'error':'用户不存在'})
+        return JsonResponse({'error':u'用户名或密码错误'})
     elif user.is_active:
         login(request, user)
     else:
-        return JsonResponse({'error':'该用户已被冻结'})
+        return JsonResponse({'error':u'该用户已被冻结'})
 
     # return HttpResponseRedirect('')
     return JsonResponse({'info':'success'})
+
 
 def sign_out(request):
     logout(request)
@@ -64,6 +70,29 @@ def sign_out(request):
     return JsonResponse({'info':'success'})
 
 
+@require_POST
+# @login_required()
 def reset_pwd(request):
+    if not request.user.is_authenticated():
+        return JsonResponse({'error':u'用户没有登录'})
 
-    return
+    old_pass = request.POST['old_pass'].strip()
+    new_pass = request.POST['new_pass'].strip()
+    new_pass_cfm = request.POST['new_pass_cfm'].strip()
+
+    if old_pass is None or new_pass is None or new_pass_cfm is None:
+        return JsonResponse({'error':u'密码不可为空'})
+
+    if new_pass != new_pass_cfm:
+        return JsonResponse({'error':u'新密码不一致'})
+    if len(new_pass) < 8:
+        return JsonResponse({'error':u'新密码长度不可少于8位'})
+
+    # if 1 == User.objects.filter(username=request.user.username).update(password=new_pass):
+    #     return JsonResponse({'info':'success'})
+
+    user = User.objects.get(username=request.user.username)
+    user.set_password(new_pass)
+    user.save()
+
+    return JsonResponse({'info':'success'})
