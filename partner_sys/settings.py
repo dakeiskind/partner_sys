cf0b7eee59f5f2039b961b9f55b49f1de42a8664
dakeiskind <dakeiskind@sina.com>
2015-07-24 15:25:23 +0800

    ....

--- partner_sys/settings.py
+++ partner_sys/settings.py
@@ -32,9 +32,9 @@ ENCODING = 'utf-8'
 # Application definition
 
 INSTALLED_APPS = (
-    'corsheaders',
-    'template_debug',
-    'material',
+    # 'corsheaders',
+    # 'template_debug',
+    # 'material',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,7 +52,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
