2577542b10574a1b43aa46ca19d76392c1b1e075
dakeiskind <dakeiskind@sina.com>
2015-08-25 17:38:31 +0800

    notices in home page

--- partner_sys/utils.py
+++ partner_sys/utils.py
@@ -14,3 +14,14 @@ def load_json(request):
         return json_obj
     except:
         print('Loading json string failed.')
+        raise ValueError(r'反序列化对象不可为空')
+
+
+def to_json(object, default=None):
+    if object is None:
+        raise ValueError(r'反序列化对象不可为空')
+
+    try:
+        return json.dump(object, default)
+    except Exception as error:
+        raise RuntimeError(r'反序列化对象失败') from error
