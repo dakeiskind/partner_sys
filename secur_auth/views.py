f17a36126bd4909e7263bb84e4d30e1276b32a83
dakeiskind <dakeiskind@sina.com>
2015-08-25 17:40:14 +0800

    notices in home page

--- secur_auth/views.py
+++ secur_auth/views.py
@@ -1,63 +1,27 @@
-__author__ = 'chenqi'
+import re
 
-from django.http import Http404, HttpRequest, HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import JsonResponse
+
 from django.views.decorators.http import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+
+from django.db import transaction
+
+from workflow.potential.flows import RegistryApprovalFlow
 from partner.models import *
-import re, json
-from flows.potential.flows import PotentialApprovalFlow
-from partner_sys import settings, utils
+from partner_sys import utils
+
 
 @require_POST
+@transaction.atomic
 def sign_up(request):
 
-    if False:
-        c = Contact()
-        c.name = 'contact_01'
-        c.title = 'no title'
-        c.tel = '110'
-        c.mobile = '13800138000'
-        c.idCard = 'mei you'
-        c.email = '1@1.com'
-        c.idCopy = 'ye mei you'
-        c.save()
+    try:
+        signup = utils.load_json(request)
+    except:
+        return JsonResponse({'error':u'参数格式有误'})
 
-        p = Potential()
-        p.username = 'potential_01'
-        p.password = '123qwe!@#'
-        p.email = '1@1.com'
-        p.zh_name = u'供应商_01'
-        p.en_name = 'partner_01'
-        p.ceo = 'Whoever'
-        p.scope = 'what?'
-        p.founding = datetime.datetime.today()
-        p.capital = 100000000000
-        p.licence = '0101010101010'
-        p.licenceCopy = 'mei you'
-        p.tax = 'mei you'
-        p.taxCopy = 'ye mei you'
-        p.orgCode = '250'
-        p.orgCodeCopy = 'mei you'
-        p.employees = 3
-        p.address = r'beijing'
-        p.homepage = 'www.bjhjyd.gov.cn'
-        p.tel = '62620202'
-        p.fax = '66668888'
-        p.post = u'北京市630信箱'
-        p.summary = 'blahblahblah....'
-        p.contact = c
-        p.save()
-
-        return JsonResponse({'contact':c.id, 'potential':p.id})
-
-    # username = request.POST['username'].strip()
-    # password = request.POST['password'].strip()
-    # password_confirmed = request.POST['password_confirmed'].strip()
-    # email = request.POST['email'].strip()
-
-    signup = utils.load_json(request)
     username = signup['username'].strip()
     password = signup['password'].strip()
     password_confirmed = signup['password_confirmed'].strip()
@@ -74,8 +38,12 @@ def sign_up(request):
     user_reg_m = username_regex.match(username)
     if user_reg_m is None or user_reg_m.group() != username:
         return JsonResponse({'error':u'用户名只能包含字母、数字及下划线，且必须字母或数字开头'})
-    if User.objects.get(username=username) is not None:
-        return JsonResponse({'error':u'此用户名已被注册'})
+    try:
+        user = User.objects.get(username=username)
+        if user is not None:
+            return JsonResponse({'error':u'此用户名已被注册'})
+    except Exception as e:
+        pass
     if password is None or password_confirmed is None:
         return JsonResponse({'error':u'密码不可为空'})
     if password_confirmed != password:
@@ -87,17 +55,20 @@ def sign_up(request):
     email_reg_m = email_regex.match(email)
     if email_reg_m is None or email_reg_m.group() != email:
         return JsonResponse({'error':u'邮箱地址格式不正确'})
-    if User.objects.get(email=email) is not None:
-        return JsonResponse({'error':u'此邮箱已被注册'})
-
-    new_user = User.objects.create_user(username, email, password)
+    try :
+        user = User.objects.get(email=email)
+        if user is not None:
+            return JsonResponse({'error':u'此邮箱已被注册'})
+    except Exception as e:
+        pass
+    new_user = User.objects.create_user(username, email=email, password=password)
+    # new_user.is_active = False
     new_user.save()
 
-
     #launch potential partner approval process
-    # PotentialApprovalFlow.start.run()
+    RegistryApprovalFlow.start.run(created_by=new_user)
 
-    return JsonResponse({'info':'success'})
+    return JsonResponse({'info':'success', 'id':new_user.id})
 
 
 @require_POST
@@ -105,6 +76,9 @@ def sign_in(request):
     signin = utils.load_json(request)
     username = signin['username'].strip()
     password = signin['password'].strip()
+
+    if username is None or password is None:
+        return JsonResponse({'error':u'缺少用户名或密码'})
 
     user = authenticate(username=username, password=password)
     if user is None:
